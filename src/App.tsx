787859e538422de37926c58e808e80ef47406efb
mashmela <mashiklavashik15@gmail.com>
2023-03-13 14:39:07 +0300

    add vocabulary logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 
-import { translate, wrapper } from "./styles.css";
+import {
+  translate,
+  wrapper,
+  vocabulary,
+  word,
+  inputWords,
+  buttonAdd,
+  imageDelete,
+  vocabularyWrapper,
+  tegs,
+  wrapperWords,
+  wrapperDelete,
+} from "./styles.css";
 
 function App() {
   const [value, setValueInput] = React.useState("");
   const [valueTranslate, setTranslateValueInput] = React.useState("");
+  const [newTranslate, setNewTranslate] = React.useState<storageTranslate[]>([
+    { first: "cat", second: "кошка" },
+  ]);
 
-  const handleInputChange = React.useCallback(
+  interface storageTranslate {
+    first: string;
+    second: string;
+  }
+
+  const handleInputWordChange = React.useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => setValueInput(event.target.value),
     [],
   );
@@ -16,22 +36,63 @@ function App() {
     [],
   );
 
-  const handleButtonClick = React.useCallback(
-    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      console.log(event.target);
-      console.log(value);
-      console.log(valueTranslate);
+  const handleButtonClick = React.useCallback(() => {
+    if (value && valueTranslate) {
+      setNewTranslate([...newTranslate, { first: value, second: valueTranslate }]);
+      setValueInput("");
+      setTranslateValueInput("");
+    }
+  }, [newTranslate, value, valueTranslate]);
+
+  const handleDeleteClick = React.useCallback(
+    (id: number) => {
+      const newStorage = [...newTranslate];
+      newStorage.splice(id, 1);
+      setNewTranslate(newStorage);
     },
-    [value, valueTranslate],
+    [newTranslate],
   );
 
   return (
     <div>
       <div className={wrapper}>
         <div className={translate}>
-          <input value={value} onChange={handleInputChange} />
-          <input value={valueTranslate} onChange={handleInputTranslateChange} />
-          <button onClick={handleButtonClick}>Add</button>
+          <input
+            className={inputWords}
+            value={value}
+            placeholder={"Слово"}
+            onChange={handleInputWordChange}
+          />
+          <input
+            className={inputWords}
+            value={valueTranslate}
+            placeholder={"Перевод"}
+            onChange={handleInputTranslateChange}
+          />
+          <button className={buttonAdd} onClick={handleButtonClick}>
+            Add
+          </button>
+        </div>
+        <div className={vocabularyWrapper}>
+          <div className={tegs}>
+            <div className={wrapperDelete}>Слово</div>
+            <div className={wrapperDelete}>Перевод</div>
+            <div className={wrapperDelete}></div>
+          </div>
+
+          {newTranslate.map((item, id) => (
+            <div className={vocabulary} key={id}>
+              <div className={wrapperWords}>
+                <div className={word}>{item.first}</div>
+              </div>
+              <div className={wrapperWords}>
+                <div className={word}>{item.second}</div>
+              </div>
+              <div className={wrapperDelete} onClick={() => handleDeleteClick(id)}>
+                <img className={imageDelete} src="/images/delete.png" alt="Delete" />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/styles.css.ts
+++ src/styles.css.ts
@@ -4,9 +4,89 @@ export const wrapper = style({
   display: "flex",
   flexDirection: "column",
   margin: "auto",
+  fontSize: "16px",
+  minWidth: "500px",
+  maxWidth: "1000px",
+  width: "100vw",
+  fontFamily: "Inter",
 });
 
 export const translate = style({
   display: "flex",
-  margin: "auto",
+  gap: "20px",
+  padding: "40px 0px",
+  justifyContent: "space-between",
+});
+
+export const inputWords = style({
+  border: "none",
+  borderBottom: "1px solid black",
+  fontSize: "24px",
+  outline: "0 !important",
+});
+
+export const buttonAdd = style({
+  border: "none",
+  fontSize: "24px",
+  width: "200px",
+  height: "50px",
+  color: "grey",
+  borderRadius: "30px",
+  cursor: "pointer",
+  transition: "0.5s",
+  ":hover": {
+    color: "white",
+    background: "black",
+  },
+});
+
+export const vocabularyWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "10px",
+});
+
+export const vocabulary = style({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+export const tegs = style({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+export const wrapperWords = style({
+  width: "35%",
+  display: "flex",
+});
+
+export const word = style({
+  display: "flex",
+  fontSize: "24px",
+  padding: "4px 20px",
+  background: "#F0F0F0",
+  borderRadius: "30px",
+  marginRight: "auto",
+});
+
+export const wrapperDelete = style({
+  width: "200px",
+});
+
+export const imageDelete = style({
+  marginLeft: "auto",
+  width: "16px",
+  height: "16px",
+  opacity: "0.5",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  cursor: "pointer",
+  transition: "0.2s",
+  ":hover": {
+    opacity: "4",
+  },
 });
